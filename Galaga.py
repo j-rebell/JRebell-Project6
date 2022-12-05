71c62e2d0140accb3e57af5845b2792c6526b4df
Jake <j1rebell@bridgew.student.edu>
2022-12-04 23:52:23 -0500

    A galaga clone made in arcade.

--- Galaga.py
+++ Galaga.py
@@ -15,13 +15,16 @@ class Galaga(arcade.Window):
         self.score = 0
         self.score_text = None
         self.lives = 3
+        #Implimented a 3 life system drawn on the bottom left of the screen
         self.lives_counter = arcade.SpriteList()
         self.enemy_speed = .5
         self.shot_speed = 5
         self.music = None
         self.game_over = None
         self.won = None
+        #Everyhting I currently need to make the game work at a basic level
     def setup(self):
+        #Ship came from the zip file. Self music ended up being unused as arcade is not happy to play any version of the file I give, mp3 or wav even in mono
         self.ship = arcade.Sprite("ship8up.png")
         self.shot_sound = arcade.load_sound(":resources:sounds/laser2.wav")
         self.score_sound = arcade.load_sound(":resources:sounds/jump3.wav")
@@ -29,11 +32,13 @@ class Galaga(arcade.Window):
         self.ship.center_y = 64
         self.music = arcade.load_sound("E1M1.wav")
         self.game_over = arcade.Text("Game Over", 400, 800, arcade.color.RED, 50)
+        #Initial drawing of enemies
         for num in range(6):
             enemy = arcade.Sprite("Enemy.png")
             enemy.center_x = random.randint(64, 736)
             enemy.center_y = random.randint(1200, 1600)
             self.enemies.append(enemy)
+        #Life counters are handled as a sprite list also
         for num in range(self.lives):
             life = arcade.Sprite("ship8up.png", .25)
             life.center_x = len(self.lives_counter) * 16 + 16
@@ -46,14 +51,17 @@ class Galaga(arcade.Window):
         self.enemies.draw()
         self.shots.draw()
         self.lives_counter.draw()
+        #Setting up the game over screen
         if self.lives < 1:
             self.game_over.draw()
         self.score_text = arcade.Text(f"{self.score * 1000}", 736, 16, arcade.color.WHITE, 16)
         self.score_text.draw()
+        #Telling the player they won
         if self.score >= 20:
             self.won = arcade.Text(f"You won!\nYour score was, {self.score * 1000}", 16, 800, arcade.color.GREEN, 25)
             self.won.draw()
         arcade.finish_render()
+        #Pyhton did not enjoy sleeping during the render. I'm sure there is a cleaner way to handle this but it works.
         if self.score >= 20:
             time.sleep(5)
             arcade.close_window()
@@ -71,6 +79,7 @@ class Galaga(arcade.Window):
                 arcade.play_sound(self.shot_sound)
             else:
                 pass
+        #These are some bindings to make testing easier. The program does not seem to recognise game over but will do game won when using the cheater keys.
         if symbol == arcade.key.ESCAPE:
             arcade.close_window()
         if symbol == arcade.key.U:
@@ -81,17 +90,22 @@ class Galaga(arcade.Window):
         if symbol == arcade.key.LEFT or symbol == arcade.key.RIGHT:
             self.ship_dx = 0
     def on_update(self, delta_time: float):
+        #Again not used but left in for attempts in the future
         if self.score >= 50:
             arcade.play_sound(self.music)
+        #Handling player borders. Not elegant and can be overcome but works well enough
         self.ship.center_x += self.ship_dx
         if self.ship.center_x <= 64 or self.ship.center_x >= 736:
             self.ship_dx = 0
+        #Used to make the enemies move towards the player
         for enemy in self.enemies:
             enemy.center_y += -self.enemy_speed
+        #Sprite list to control the laser sprite list. Speed can easily be changed for fine tuning.
         for shot in self.shots:
             shot.center_y += self.shot_speed
             if shot.center_y > 1632:
                 self.shots.remove(shot)
+            #Handling when the player kills an enemy
             enemy_death = arcade.check_for_collision_with_list(shot, self.enemies)
             if enemy_death:
                 for enemy in enemy_death:
@@ -104,15 +118,18 @@ class Galaga(arcade.Window):
                     new_enemy.center_y = random.randint(1600, 2000)
                     self.enemies.append(new_enemy)
                     print(self.score)
+        #Finding out whether to subtract a life from the player
         life_lost = arcade.check_for_collision_with_list(self.ship, self.enemies)
         if life_lost:
             rem_life_x = int(self.lives * 16)
+            #Removing the sprite caused a lot of frustration as get sprites at point would not return anyhting
             get_life = arcade.get_sprites_at_point([rem_life_x, 16], self.lives_counter)
             rem_life = get_life[-1]
             for enemy in life_lost:
                 self.lives -= 1
                 self.enemies.remove(enemy)
                 self.lives_counter.remove(rem_life)
+        #Closing the window after a loss
         if self.lives < 1:
             time.sleep(5)
             arcade.close_window()
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+#Jacob Rebell
+
 import arcade
 from Galaga import Galaga
-
+# From the imported file, get the window ready to be shown
 def main():
     win = Galaga()
     win.setup()
